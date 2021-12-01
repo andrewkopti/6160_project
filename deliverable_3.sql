617888d1685dd221417e484b8a2227e9047ce970
Andrew Kopti <andrewkopti@gmail.com>
2021-12-01 11:28:10 -0500

    added stored procedure to calculate rating

--- deliverable_3.sql
+++ deliverable_3.sql
@@ -1,5 +1,34 @@
-CREATE VIEW driver_ratings AS
+-- stored procedure to calculate ratings
+DELIMITER //
+DROP PROCEDURE IF EXISTS calculate_ratings;
+CREATE PROCEDURE calculate_ratings ()
+BEGIN
+	SELECT d.driver_id as id,
+	"driver" AS type,
+    AVG(r.num_stars) AS rating
+    FROM driver AS d
+    JOIN `order` AS o
+	ON o.driver_id = d.driver_id
+	JOIN `rating` AS r
+	ON r.rating_id = o.driver_rating
+    GROUP BY d.driver_id
+    UNION
+    SELECT re.restaurant_id as id,
+	"restaurant" AS type,
+    AVG(r.num_stars) AS rating
+    FROM restaurant AS re
+    JOIN `order` AS o
+	ON o.restaurant_id = re.restaurant_id
+	JOIN `rating` AS r
+	ON r.rating_id = o.restaurant_rating
+    GROUP BY re.restaurant_id;
+END//
+DELIMITER ;
 
+CALL calculate_ratings;
+
+-- advanced view
+CREATE OR REPLACE VIEW driver_rating_count AS
 WITH cte AS (
 SELECT d.driver_id AS driver_id,
 	r.rating_id AS rating_id,
@@ -26,4 +55,4 @@ FROM cte
 GROUP BY driver_id
 ORDER BY avg_rating;
 
-SELECT * FROM driver_ratings;
+SELECT * FROM driver_rating_count;
